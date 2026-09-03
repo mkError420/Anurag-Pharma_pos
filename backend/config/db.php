6a6cddec07d7831a3c1cf22078e3e977769cf96d
mkError420 <mk.rabbani.cse@gmail.com>
2026-09-03 15:49:05 +0600

    cpanel database error solved

--- backend/config/db.php
+++ backend/config/db.php
@@ -80,6 +80,7 @@ class DB {
                 PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 PDO::ATTR_EMULATE_PREPARES   => false,
+                PDO::ATTR_TIMEOUT            => 5, // 5s timeout prevents PHP-FPM workers from hanging
             ];
  
             try {
